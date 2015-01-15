0813146354ee03a2d1c4e081e6abdfd296b974fa
Roy Marquez <theroyal3655@gmail.com>
2015-01-15 14:04:49 -0800

    Almost kinda done

--- public_html/script.js
+++ public_html/script.js
@@ -5,7 +5,16 @@
 
 
 $("document").ready(function(){
-    $("h1, p").css("background-color", "purple");
+    $(".yeah").css("background-color", "purple");
+    
+    $('#superHumans').accordion({header: "h3"});
+     $('#superHumans').css('width', '400px');
+     
+    $('h3').css('background', 'cyan');
+    
+    $('.2').css({'visability': 'hidden', 'position': 'absolute'});
+    
+    $('div:last p:nth-child(3)').css('background-color', 'pink');
     
     $("h2").bind('mouseover', mouseOverMe).bind ('mouseout', mouseOutMe);
     
@@ -13,8 +22,43 @@ $("document").ready(function(){
     
     $('p').bind('mouseover', mouseOverMe());
     
+    $('#replaceWText').bind('click', replaceWText);
+    
+    $('#randPara').bind('click', addAPara);
+    
+    $('#removePara').bind('click', removePara);
+    
+    $('#show').bind('visability', 'hidden');
+    
+    $('#hide').bind('click', hideThePage);
+    
+    $('#show').bind('click', showThePage);
+    
     
 });
+
+function hideThePage() {
+    $('#show').css('visability', 'visable');
+    $('#div').hide('slide', {}, 2500);
+    $('#show').show('fold', {}, 2500);
+}
+
+function showThePage() {
+    $('div').show('fold', {}, 2500);
+    $('#show').hide('puff', {}, 2500);
+}
+
+function removeAPara() {
+    $('#randPara p:last').remove();
+}
+
+function addAPara() {
+    $('#randPara').append('<p>ADDED</p>');
+}
+
+function replaceWText() {
+    $('#randPara').html('<h1>Danny loves Twilight</h1>');
+}
 
 function mouseOverMe() {
     $('h2').html("this is easy");
